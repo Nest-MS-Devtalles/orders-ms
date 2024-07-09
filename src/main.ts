aa8109c8b1a02a469f657c140555d8bb0a81ad30
valdo <osvaldo@lvvdigital.com>
2024-07-09 16:19:46 -0600

    feat: base orders resource

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main-Order');
-  const app = await NestFactory.create(AppModule);
 
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  await app.listen();
   logger.log(`Order-MS is running on: http://localhost:${envs.port}`);
 }
 bootstrap();
