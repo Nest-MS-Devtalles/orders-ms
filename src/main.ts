45db600f82499566a0e0851e6527593dcb020477
valdo <osvaldo@lvvdigital.com>
2024-07-09 16:09:46 -0600

    feat: config envs vars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Order');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(envs.port);
+  logger.log(`Order-MS is running on: http://localhost:${envs.port}`);
 }
 bootstrap();
